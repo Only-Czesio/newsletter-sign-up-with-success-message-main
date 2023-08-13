49ea8cf7db3298b318c2e6ca462311b0e6124b88
Only-Czesio <marek@kamyk.me>
2023-08-13 17:04:16 +0200

    Inicial dump of all the files.

---
+++ newsletter/javascript.js
@@ -0,0 +1,29 @@
+let connecting = document.querySelector("#email");
+let button = document.querySelector("#subscribe");
+let form = document.querySelector("#anything");
+let success = document.querySelector(".success");
+
+// if user input wrong email address so basically doesnt have a dot, a sign --> @ then i don't allow to go to success message
+//if its correct then apply function success message which send You to new link success.html
+//
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+  document.querySelector(".background").classList.add("hidden");
+  success.classList.remove("hidden");
+  
+})
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+    if(connecting.value === "") 
+    if (!validateEmail(connecting.value)) {
+        
+    }
+}
+function validateEmail(email) {
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return regex.test(String(email).toLowerCase());
+}
+
+
+  
