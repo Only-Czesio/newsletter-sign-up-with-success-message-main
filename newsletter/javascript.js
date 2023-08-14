8a71d216e05bb8bf7f1533f0fff01178952650b5
Marek <74934496+Only-Czesio@users.noreply.github.com>
2023-08-14 14:27:10 +0000

    Given style to error when wrong input.
    Repeared disabled button, because earlier was disabled for good.

--- newsletter/javascript.js
+++ newsletter/javascript.js
@@ -2,25 +2,34 @@ let connecting = document.querySelector("#email");
 let button = document.querySelector("#subscribe");
 let form = document.querySelector("#anything");
 let success = document.querySelector(".success");
-
+let errorMessage = document.querySelector(".disabled");
 // if user input wrong email address so basically doesnt have a dot, a sign --> @ then i don't allow to go to success message
 //if its correct then apply function success message which send You to new link success.html
 //
 form.addEventListener("submit", function (event) {
-  console.log("I successfully show you a success message");
-  event.preventDefault();
-  document.querySelector(".background").classList.add("hidden");
-  success.classList.remove("hidden");
-  
-})
+  if (connecting.value.trim() === "") {
+    console.log("Input is empty");
+    event.preventDefault();
+    errorMessage.classList.remove("disabled");
+  } else if (!isValidEmail(connecting.value)) {
+    console.log("Invalid email address");
+    event.preventDefault();
+  } else {
+    console.log("I successfully show you a success message");
+    document.querySelector(".background").classList.add("hidden");
+    success.classList.remove("hidden");
+  }
+});
 button.addEventListener("click", handleClick);
 
 function handleClick() {
   console.log("I am handling the button")
-  if (connecting.value.trim() === "") {
-      console.log("I should disable button if input is empty");
-    button.disabled = true;
-  }
-  let value = document.querySelector("#confirmationEmail");
-  value.innerHTML = connecting.value;
+    let value = document.querySelector("#confirmationEmail");
+    value.innerHTML = connecting.value;
 }
+  function isValidEmail(email) {
+    // Simple regex for email validation
+    var re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
