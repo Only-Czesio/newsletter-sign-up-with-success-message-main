ea29aa85c82086aeca4b9e65dc51de5f44a2c6da
Marek <74934496+Only-Czesio@users.noreply.github.com>
2023-08-14 14:01:16 +0000

    Disabled button if input is empty, changed cursor to pointer for inserting email address

--- newsletter/javascript.js
+++ newsletter/javascript.js
@@ -6,8 +6,9 @@ let success = document.querySelector(".success");
 // if user input wrong email address so basically doesnt have a dot, a sign --> @ then i don't allow to go to success message
 //if its correct then apply function success message which send You to new link success.html
 //
-form.addEventListener("submit", function(event){
-    event.preventDefault();
+form.addEventListener("submit", function (event) {
+  console.log("I successfully show you a success message");
+  event.preventDefault();
   document.querySelector(".background").classList.add("hidden");
   success.classList.remove("hidden");
   
@@ -15,8 +16,10 @@ form.addEventListener("submit", function(event){
 button.addEventListener("click", handleClick);
 
 function handleClick() {
-  if (connecting.value === "") {
-      return
+  console.log("I am handling the button")
+  if (connecting.value.trim() === "") {
+      console.log("I should disable button if input is empty");
+    button.disabled = true;
   }
   let value = document.querySelector("#confirmationEmail");
   value.innerHTML = connecting.value;
